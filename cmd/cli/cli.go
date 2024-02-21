d142e9a87b35ed56aa606ce2dd3973ada17c7dae
Marco Antônio <ma.souza.junior@gmail.com>
2024-02-21 17:09:35 -0300

    fea: setup CLI to get AI instructions

---
+++ cmd/cli/cli.go
@@ -0,0 +1,86 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	textarea textarea.Model
+	err      error
+}
+
+func MakeModel() model {
+	ti := textarea.New()
+	ti.Placeholder = " You are Mario from Super Mario Bros, acting as an assistant..."
+	ti.Focus()
+
+	return model{
+		textarea: ti,
+		err:      nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return tea.Batch(tea.ClearScreen, textarea.Blink)
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		// select msg type
+		switch msg.Type {
+
+		case tea.KeyEsc:
+			if m.textarea.Focused() {
+				m.textarea.Blur()
+			}
+
+		case tea.KeyEnter:
+			if !m.textarea.Focused() {
+				fmt.Println("Done!")
+
+				value := m.textarea.Value()
+				fmt.Println(value)
+
+				return m, tea.Quit
+			}
+
+		case tea.KeyCtrlC:
+			return m, tea.Quit
+
+		default:
+			if !m.textarea.Focused() {
+				cmd = m.textarea.Focus()
+				cmds = append(cmds, cmd)
+			}
+
+		}
+
+	case error:
+		// return if msg is an error
+		m.err = msg
+		return m, nil
+	}
+
+	m.textarea, cmd = m.textarea.Update(msg)
+	cmds = append(cmds, cmd)
+	return m, tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	avaliableHotkeys := "(ctrl+c) to quit, (esc) to save"
+	if !m.textarea.Focused() {
+		avaliableHotkeys = "(enter) to save"
+	}
+	return fmt.Sprintf(
+		"Tell me what you need.\n\n%s\n\n%s",
+		m.textarea.View(),
+		avaliableHotkeys,
+	) + "\n\n"
+}
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/marco-souza/omg/cmd/cli"
+)
 
 func main() {
-	fmt.Println("Ollama Modelfile Generator")
+	p := tea.NewProgram(cli.MakeModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
