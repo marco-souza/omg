5f5ddae3bd8fadb3853d45b43b69a009663a5fe1
Marco Antônio <ma.souza.junior@gmail.com>
2024-03-21 20:14:52 -0300

    fea: use CLI params

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/marco-souza/omg/cmd/cli"
+	"github.com/marco-souza/omg/internal/llm"
 )
 
 func main() {
+	argsWithoutProg := os.Args[1:]
+
+	if len(argsWithoutProg) > 0 {
+		prompt := strings.Join(argsWithoutProg, " ")
+		llm.Completion(prompt)
+		return
+	}
+
 	p := tea.NewProgram(cli.MakeModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
